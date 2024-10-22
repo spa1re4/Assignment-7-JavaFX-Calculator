a2881f05312710c8ec090202bf13c6d6b7e990ca
spa1re4 <150504411+spa1re4@users.noreply.github.com>
2024-10-22 20:54:05 +0600

    Add files via upload

--- Calculator/src/main/java/org/example/calculator/HelloController.java
+++ Calculator/src/main/java/org/example/calculator/HelloController.java
@@ -6,18 +6,36 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
+    calculator calculator = new calculator();
+    private boolean isOperatorPressed;
 
+    {
+        isOperatorPressed = false;
+    }
+
+    private boolean isResultDisplayed;
+    {
+        isResultDisplayed = false;
+    }
     @FXML
     private TextField txt_result;
-
-    private calculator calculator = new calculator();
 
     @FXML
     void Number(ActionEvent event) {
         Button button = (Button) event.getSource();
         String number = button.getText();
+        if(isResultDisplayed)
+        {
+            txt_result.setText("");// Очищаем поле если уже есть результат
+            isResultDisplayed = false;
+        }
+        if(isOperatorPressed)
+        {
+            txt_result.setText("");// Очищаем поле после оператора для использования 2
+            isOperatorPressed = false;
+        }
         String oldText = txt_result.getText();
-        String newText = oldText+ number;
+        String newText = oldText + number;
         txt_result.setText(newText);
     }
 
@@ -25,25 +43,43 @@ public class HelloController {
     void Operation(ActionEvent event) {
         Button button = (Button) event.getSource();
         String operation = button.getText();
+        String oldText = txt_result.getText();
+        calculator.setOperand1(Double.parseDouble(txt_result.getText()));
+        calculator.setOperator(operation.charAt(0));
+        isOperatorPressed=true;
+        isResultDisplayed=true;
     }
 
     @FXML
     void equals(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        String oldText = txt_result.getText();
-        String newText = oldText + button.getText();
-        txt_result.setText(newText);
+        // Устанавливаем второй операнд и выполняем вычисление
+        calculator.setOperand2(Double.parseDouble(txt_result.getText()));
+
+        if (calculator.isError()) {
+            txt_result.setText("ERROR");
+        } else txt_result.setText(String.valueOf(calculator.getResult()));
+
+        isResultDisplayed = true;  // Отображен результат, можно начать новый расчет
     }
-    @FXML
-    void point(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        String point = button.getText();
-        String oldText = txt_result.getText();
-        if (!oldText.contains(".")) { // Проверяем если точка уже или нет, если есть то не добавляем
-            String newText = oldText + point;//если нет, то добавляем
-            txt_result.setText(newText);
+
+        @FXML
+        void point (ActionEvent event){
+            Button button = (Button) event.getSource();
+            String point = button.getText();
+            String oldText = txt_result.getText();
+            if (!oldText.contains(".")) { // Проверяем если точка уже или нет, если есть то не добавляем
+                String newText = oldText + point;//если нет, то добавляем
+                txt_result.setText(newText);
             }
-    }
+        }
+        @FXML
+        void clear (ActionEvent event)
+        {
+            txt_result.setText("");
+            isResultDisplayed=false;
+            isOperatorPressed=false;
+
+        }
 }
 
 
--- Calculator/src/main/java/org/example/calculator/calculator.java
+++ Calculator/src/main/java/org/example/calculator/calculator.java
@@ -1,59 +1,46 @@
 package org.example.calculator;
 
 public class calculator {
-    private int op1;
-    private int op2;
+    private double operand1;
+    private double operand2;
     private char operator;
-    private int result;
-    private boolean isError;
-
-    public int getOp1() {
-        return op1;
+    private boolean error;
+    public void setOperand1(double operand1) {
+        this.operand1 = operand1;
     }
-
-    public void setOp1(int op1) {
-        this.op1 = op1;
+    public void setOperand2(double operand2) {
+        this.operand2 = operand2;
     }
-
-    public int getOp2() {
-        return op2;
-    }
-
-    public void setOp2(int op2) {
-        this.op2 = op2;
-    }
-
-    public char getOperator() {
-        return operator;
-    }
-
     public void setOperator(char operator) {
         this.operator = operator;
     }
-
-    public int getResult() {
+    public boolean isError() {
+        return error;
+    }
+    public double getResult() {
+        error = false;
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*':
+                result = operand1 * operand2;
+                break;
+            case '/':
+                if (operand2 != 0) {
+                    result = operand1 / operand2;
+                } else {
+                    error = true;
+                }
+                break;
+            default:
+                error = true;
+                break;
+        }
         return result;
     }
-    public void calculate() {
-        if(this.operator == '+') {
-            this.result = this.op1 + this.op2;
-        }
-        else if(this.operator == '-') {
-            this.result = this.op1 - this.op2;
-        }
-        else if(this.operator == '*') {
-            this.result = this.op1 * this.op2;
-        }
-        else if(this.operator == '/') {
-            this.result = this.op1 / this.op2;
-            if(this.op2==0)
-            {
-                this.isError = true;
-            }
-            else {
-                this.isError = false;
-            }
-            }
-        }
 }
-
